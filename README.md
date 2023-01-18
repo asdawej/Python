ac92e8e8bc3073ac05badb01fc1db07dca7d9cac
964293341 <asdawej123@163.com>
2023-01-18 17:10:04 +0800

    RESTART: Matrix
    - ADD __getitem__ method,
    -- Like numpy, but index start from 1 instead of 0.
    - REVISE README

--- README.md
+++ README.md
@@ -10,22 +10,22 @@ In hand but now do not doing now.
 
 ## __modules/Matrix
 
-In hand, no progress.
+In hand.
 
-## FAIL/Curve_Fit
+## FAILED/Curve_Fit
 
 First practice of machine learning, half-success and half-failure.
 
-## FAIL/Evolution_Simulator
+## FAILED/Evolution_Simulator
 
 Practice of evoultion model, failure with unknown reason.
 
-## FAIL/Test_Learning_Model
+## FAILED/Test_Learning_Model
 
 Reasonable designed but do not get the expected effect.
 
 
-## FAIL/Module_Lines_Dictionary
+## FAILED/Module_Lines_Dictionary
 
 A project started with the aim of learning new language and modules and to store, arrange, and search in lines.
 
--- __modules/Matrix.py
+++ __modules/Matrix.py
@@ -1,3 +1,5 @@
+from typing import Any, NoReturn
+from types import EllipsisType
 
 __author__ = 'asdawej'
 
@@ -11,67 +13,201 @@ class Shape(object):
 
 
 class Matrix(object):
-    '''Members are /mat/, /shape.m/, /shape.n/ and /block/\n
-    Methods are /element/, /row/, /column/'''
+    '''
+    Members are /mat/, /shape.m/, and /shape.n/\n
+    Methods are /element/, /row/, /column/
+    '''
 
-    def __init__(self, mat: list[list]):
-        '''Automatically, we will count the shape and block\n
-        Members are /mat/, /shape.m/, /shape.n/ and /block/'''
+    def __init__(self, mat: list[list]) -> NoReturn:
+        '''
+        Automatically, we will count the shape\n
+        Members are /mat/, /shape.m/, and /shape.n/
+        '''
         s = len(mat[0])
         for x in mat:
             if len(x) != s:
                 raise ValueError
         self.mat = mat
         self.shape = Shape(m=len(mat), n=s)
-        self.block = 0
-        for x in mat:
-            for y in x:
-                if isinstance(y, Matrix):
-                    if y.block > self.block:
-                        self.block = y.block
 
-    def element(self, i: int, j: int):
-        '''Index from 1 to m, from 1 to n'''
-        if i <= 0 or i > self.shape.m or j <= 0 or j > self.shape.n:
-            raise IndexError
-        return self.mat[i-1][j-1]
+    def __getitem__(
+        self, key: int | slice | EllipsisType | tuple[int | slice | EllipsisType]
+    ) -> Any | 'Matrix' | NoReturn:
+        '''
+        Index from 1 to m, from 1 to n\n
+        - If int, return an element or a row\n
+        - If slice, always return a Matrix\n
+            - If self.shape.m == 1, return a row vector\n
+            - Else, return a Matrix of rows\n
+        - If Ellipsis, return a copy\n
+        - If tuple, return an element or a Matrix\n
+            - One row:\n
+                - One column, return an element\n
+                - Slice columns, return a row vector\n
+                -Ellipsis, return a row\n
+            - Slice rows:\n
+                - One column, return a column vector\n
+                - Slice columns, return a general Matrix\n
+                - Ellipsis, return a Matrix of rows\n
+            - Ellipsis:\n
+                - One column, return a coulumn\n
+                - Slice columns, return a Matrix of columns\n
+                - Ellipsis, return a copy\n
+        - Default, TypeError
+        '''
+        # If int, return an element or a row
+        if isinstance(key, int):
+            if self.shape.m == 1:
+                return self.mat[0][key-1]
+            else:
+                return Matrix([self.mat[key-1][:]])
 
-    def row(self, i: int):
-        '''The row i of it, index from 1 to m'''
-        if i <= 0 or i > self.shape.m:
-            raise IndexError
-        return Matrix([a.mat[i-1]])
+        # If slice, always return a Matrix
+        elif isinstance(key, slice):
+            if key.start:
+                key = slice(key.start-1, key.stop, key.step)
+            if key.stop:
+                key = slice(key.start, key.stop-1, key.step)
+            # If m == 1, return a row vector
+            if self.shape.m == 1:
+                return Matrix([self.mat[0][key]])
 
-    def column(self, j: int):
-        '''The column j of it, index from 1 to n'''
-        if j <= 0 or j > self.shape.n:
-            raise IndexError
-        return Matrix([[x[j-1]] for x in self.mat])
+            # Else, return a Matrix of rows
+            else:
+                return Matrix([_[:] for _ in self.mat[key]])
+
+        # If Ellipsis, return a copy
+        elif key == Ellipsis:
+            return Matrix([_[:] for _ in self.mat])
+
+        # If tuple, return an element or a Matrix
+        elif isinstance(key, tuple):
+            key = list(key)
+            # One row:
+            if isinstance(key[0], int):
+                # One column, return an element
+                if isinstance(key[1], int):
+                    return self.mat[key[0]-1][key[1]-1]
+
+                # Slice columns, return a row vector
+                elif isinstance(key[1], slice):
+                    if key[1].start:
+                        key[1] = slice(key[1].start-1, key[1].stop, key[1].step)
+                    if key[1].stop:
+                        key[1] = slice(key[1].start, key[1].stop-1, key[1].step)
+                    return Matrix([self.mat[key[0]-1][key[1]]])
+
+                # Ellipsis, return a row
+                elif key[1] == Ellipsis:
+                    return Matrix([self.mat[key[0]-1][:]])
+
+                # Default, TypeError
+                else:
+                    raise TypeError
+
+            # Slice rows:
+            elif isinstance(key[0], slice):
+                if key[0].start:
+                    key[0] = slice(key[0].start-1, key[0].stop, key[0].step)
+                if key[0].stop:
+                    key[0] = slice(key[0].start, key[0].stop-1, key[0].step)
+                # One column, return a column vector
+                if isinstance(key[1], int):
+                    temp: list[list] = []
+                    for i in range(self.shape.m)[key[0]]:
+                        temp.append([self.mat[i][key[1]-1]])
+                    return Matrix(temp)
+
+                # Slice columns, return a general Matrix
+                elif isinstance(key[1], slice):
+                    if key[1].start:
+                        key[1] = slice(key[1].start-1, key[1].stop, key[1].step)
+                    if key[1].stop:
+                        key[1] = slice(key[1].start, key[1].stop-1, key[1].step)
+                    temp: list[list] = []
+                    flag = 0
+                    for i in range(self.shape.m)[key[0]]:
+                        temp.append([])
+                        for j in range(self.shape.n)[key[1]]:
+                            temp[flag].append(self.mat[i][j])
+                        flag += 1
+                    return Matrix(temp)
+
+                # Ellipsis, return a Matrix of rows
+                elif key[1] == Ellipsis:
+                    return Matrix([_[:] for _ in self.mat[key[0]]])
+
+                # Default, TypeError
+                else:
+                    raise TypeError
+
+            # Ellipsis:
+            elif key[0] == Ellipsis:
+                # One column, return a coulumn
+                if isinstance(key[1], int):
+                    temp: list[list] = []
+                    for i in range(self.shape.m):
+                        temp.append(self.mat[i][key[1]-1])
+                    return Matrix(temp)
+
+                # Slice columns, return a Matrix of columns
+                elif isinstance(key[1], slice):
+                    if key[1].start:
+                        key[1] = slice(key[1].start-1, key[1].stop, key[1].step)
+                    if key[1].stop:
+                        key[1] = slice(key[1].start, key[1].stop-1, key[1].step)
+                    temp: list[list] = []
+                    flag = 0
+                    for i in range(self.shape.m):
+                        temp.append([])
+                        for j in range(self.shape.n)[key[1]]:
+                            temp[flag].append(self.mat[i][j])
+                        flag += 1
+                    return Matrix(temp)
+
+                # Ellipsis, return a copy
+                elif key[1] == Ellipsis:
+                    return Matrix([_[:] for _ in self.mat])
+
+                # Default, TypeError
+                else:
+                    raise TypeError
+
+            # Default, TypeError
+            else:
+                raise TypeError
+
+        # Default, TypeError
+        else:
+            raise TypeError
 
     def __str__(self):
-        '''To print the matrix'''
+        '''
+        To print the Matrix
+        '''
         s = '['
         for i in range(self.shape.m):
-            s += str(self.row(i+1).mat[0])
+            for j in range(self.shape.n):
+                s += str(self.mat[i][j])
+                if j != self.shape.n-1:
+                    s += ', '
             if i != self.shape.m-1:
                 s += '\n'
         s += ']'
         return s
 
+
+"""
     def enblock(self, row_block: list[int], column_block: list[int]):
         '''Enblock matrix according to the /row_block/ and /column_block/, as:\n
         /#Mat=[[0, 1, 2], [3, 4, 5]], row_block=[1, 1], column_block=[1, 2]\n
         ->[[0, Mat_1], [3, Mat_2]], Mat_1=[[1, 2]], Mat_2=[[4, 5]]#/'''
         # _m=[[None for x in column_block] for y in row_block]
         pass
+"""
 
 
 # Test
 if __name__ == '__main__':
     a = Matrix([[0, 1, 2], [3, 4, 5]])
-    print(a)
-    print(a.block)
-    print(a.shape.m, a.shape.n)
-    print(a.element(1, 1), a.element(2, 3))
-    print(a.row(1), a.row(2))
-    print(a.column(1), a.column(3))
+    print(a[...,3:1:-1])
